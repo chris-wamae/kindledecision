df39e38a91543a18d7c3de5d8f338d7898f9b4dc
chris-wamae <chriswamae123@gmail.com>
2023-11-27 10:33:42 +0300

    Created landing page

--- front-end/src/views/Landingpage.js
+++ front-end/src/views/Landingpage.js
@@ -1,10 +1,36 @@
 import Navbar from "../components/Navbar"
+import "../styles/Landingpage.css"
 
 function Landingpage(){
     const navItems = ["Features", "Login", "How it Works"]
     return (
      <>
      <Navbar navItems={navItems}/>
+     <div class="main">
+
+      <div class="info">
+
+        <div class="info-text">
+
+            <div class="main-header">
+                The easiest way to make group decisions
+            </div>
+
+            <div class="sub-header">
+                Casta Vote makes voting on anything increadibly simple while maintaining credibility and fairness.
+            </div>
+
+        </div>
+
+        <div class="info-img">
+        </div>
+
+      </div>
+
+      <div class="buttons">
+      </div>
+
+     </div>
      </>
     )
 }
