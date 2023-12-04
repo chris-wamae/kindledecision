d04daecea0578c8801ac75512434b838ec3d3393
chris-wamae <chriswamae123@gmail.com>
2023-12-04 07:32:35 +0300

    Updated landing page and navbar

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,13 +1,21 @@
 import "../styles/Navbar.css"
+import { Link } from "react-router-dom";
 function Navbar({navItems}){
      
+    function LinkFormater(rawLink){
+    
+    let formattedLink = rawLink.replace(/ /g,"-")
+    console.log(formattedLink.toLowerCase())
+    return formattedLink.toLowerCase()
+    }
     return (
         <>
         <div class="nav-div">
             <img src="Casta-vote-text.png" id="app-icon"></img>                
             <div class="nav-links">
             {navItems.map(e => {
-             return <a href="#">{e}</a>
+            
+             return <Link exact to={LinkFormater(e)}><a href="#">{e}</a></Link>
             })}
             </div>
         </div>
