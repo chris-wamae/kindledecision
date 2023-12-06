0b6762fd079ed06d3007084ecf2b0e79d73ff64a
chris-wamae <chriswamae123@gmail.com>
2023-12-06 07:18:45 +0300

    Updated features and how it works page

--- front-end/src/views/HowItWorkspage.js
+++ front-end/src/views/HowItWorkspage.js
@@ -11,41 +11,69 @@ function HowItWorkspage() {
             <section>
                 <div className="how-it-works-title">How it works</div>
                 <div>
-                    <div>
-                        <div className="single-hiw-element">
-                            <div>Creating an account/login</div>
+                    <div className="single-hiw-element">
+                        <div>Creating an account/login</div>
 
-                            <img src="download.png" onClick={() => setHideText(!hideText)}></img>
-                        </div>
-                        <div id={hideText ? "hide-text" : ""} className="hiw-text">
-                            The quick brown fox jumped over the lazy dog.
-                            The quick brown fox jumped over the lazy dog.
-                            The quick brown fox jumped over the lazy dog.
-                            The quick brown fox jumped over the lazy dog.
-                        </div>
+                        <img src="download.png" onClick={() => setHideText(!hideText)}></img>
+                    </div>
+                    <div id={hideText ? "hide-text" : ""} className="hiw-text">
+                        The quick brown fox jumped over the lazy dog.
+                        The quick brown fox jumped over the lazy dog.
+                        The quick brown fox jumped over the lazy dog.
+                        The quick brown fox jumped over the lazy dog.
                     </div>
 
                     <div className="single-hiw-element" id="middle-hiw-element">
                         <div>Participating in voting</div>
+
                         <img src="download.png" onClick={() => setHideTextTwo(!hideTextTwo)}></img>
+                    </div>
+                    <div id={hideTextTwo ? "hide-text" : ""} className="hiw-text">
+                        The quick brown fox jumped over the lazy dog.
+                        The quick brown fox jumped over the lazy dog.
+                        The quick brown fox jumped over the lazy dog.
+                        The quick brown fox jumped over the lazy dog.
+                    </div>
+
+
+                    <div className="single-hiw-element">
+                        <div>Email notifications</div>
+
+                        <img src="download.png" onClick={() => setHideTextThree(!hideTextThree)}></img>
+                    </div>
+                    <div id={hideTextThree ? "hide-text" : ""} className="hiw-text">
+                        The quick brown fox jumped over the lazy dog.
+                        The quick brown fox jumped over the lazy dog.
+                        The quick brown fox jumped over the lazy dog.
+                        The quick brown fox jumped over the lazy dog.
+
+                    </div>
+
+                    {/* <div className="single-hiw-element" id="middle-hiw-element">
+                        <div>
+                            <div>Participating in voting</div>
+                            <img src="download.png" onClick={() => setHideTextTwo(!hideTextTwo)}></img>
+                        </div>
                         <div id={hideTextTwo ? "hide-text" : ""} className="hiw-text">
                             The quick brown fox jumped over the lazy dog.
                             The quick brown fox jumped over the lazy dog.
                             The quick brown fox jumped over the lazy dog.
                             The quick brown fox jumped over the lazy dog.
                         </div>
-                    </div>
+                    </div> */}
 
-                    <div className="single-hiw-element">
-                        <div>Email notifications</div>
+                    {/* <div className="single-hiw-element">
+                        <div>
+                            <div>Email notifications</div>
                             <img src="download.png" onClick={() => setHideTextThree(!hideTextThree)}></img>
-                            <div id={hideTextThree ? "hide-text" : ""} className="hiw-text">
-                                The quick brown fox jumped over the lazy dog.
-                                The quick brown fox jumped over the lazy dog.
-                                The quick brown fox jumped over the lazy dog.
-                                The quick brown fox jumped over the lazy dog.
-                            </div>
-                    </div>
+                        </div>
+                        <div id={hideTextThree ? "hide-text" : ""} className="hiw-text">
+                            The quick brown fox jumped over the lazy dog.
+                            The quick brown fox jumped over the lazy dog.
+                            The quick brown fox jumped over the lazy dog.
+                            The quick brown fox jumped over the lazy dog.
+                        </div>
+                    </div> */}
                 </div>
             </section>
         </>
