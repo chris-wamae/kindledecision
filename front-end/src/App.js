620aca4bfe88fa0053e564e28ea272ff23e7a8b3
chris-wamae <chriswamae123@gmail.com>
2023-12-06 07:40:37 +0300

    Created Login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Landingpage from './views/Landingpage';
 import Featurespage from './views/Featurespage';
 import Signup from './views/Signup';
+import Login from './views/Login';
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route exact path="/" element={<Landingpage/>}></Route>
       <Route exact path="/features" element={<Featurespage/>}></Route>
       <Route exact path="/sign-up" element={<Signup/>}></Route>
+      <Route exaxt path="/login" element={<Login/>}></Route>
       </Routes> 
      </Router>
    </>
