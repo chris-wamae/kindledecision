9ad232df9c20874a7ac6143f8a984d2338b365d2
chris-wamae <chriswamae123@gmail.com>
2023-12-06 07:40:37 +0300

    Created Login page

---
+++ front-end/src/views/Login.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+import Authetication from "../components/Authentication";
+function Login() {
+    const authTitle = " Login to your account"
+    const authType = false
+    const passwordHeader = "Enter your password:"
+    const buttonText = "Sign in"
+    const navItems = ["Features", "Login", "How it Works"]
+
+    return (
+        <>
+          <Navbar navItems={navItems}/>
+          <Authetication authType={authType} authTitle={authTitle} passwordHeader={passwordHeader} buttonText={buttonText}/>
+        </>)
+}
+export default Login;
