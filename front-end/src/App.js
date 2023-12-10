0a07600a1c4e258951ab9f8b39f0de40e6d2766e
chris-wamae <chriswamae123@gmail.com>
2023-12-10 15:31:27 +0300

    Updated account-management page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import Featurespage from './views/Featurespage';
 import Signup from './views/Signup';
 import Login from './views/Login';
 import UserDashboard from './views/UserDashboard';
+import AccountManagement from './views/AccountManagement';
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
       <Route exact path="/features" element={<Featurespage/>}></Route>
       <Route exact path="/sign-up" element={<Signup/>}></Route>
       <Route exaxt path="/login" element={<Login/>}></Route>
-      <Route exact path="/user-dashboard" element={<UserDashboard/>}></Route>
+      <Route exact path="/dashboard" element={<UserDashboard/>}></Route>
+      <Route exact path="/account-management" element={<AccountManagement/>}></Route>
       </Routes> 
      </Router>
    </>
