c1952e3adb6ba99f8b6dc2048ce5391d5258cf2c
chris-wamae <chriswamae123@gmail.com>
2024-01-22 17:16:19 +0300

    Added input validation to create-election page

--- front-end/src/views/CreateElection.js
+++ front-end/src/views/CreateElection.js
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import "../styles/CreateElection.css"
 import { useState } from "react";
 import { useRef } from "react";
 function CreateElection() {
@@ -18,6 +19,16 @@ function CreateElection() {
   const [electionChoices, setElectionChoices] = useState([])
 
   const [electionVoters, setElectionVoters] = useState([])
+
+  const canSave = () => {
+  if(electionTitle !== "" && electionChoices.length !== 0 && electionVoters.length !== 0)
+  {
+    return false
+  } 
+  else{
+    return true
+  }
+  }
 
   const getTime = () => {
 
@@ -56,22 +67,45 @@ function CreateElection() {
     <>
       <Navbar navItems={navItems} />
 
-      <div className="page-header"></div>
+      <div className="page-header">Create an election</div>
 
       <div className="election-container">
 
         <div className="choice-info">
           {
             <>
-              <div className="choice-info-title">Current choices</div>
               {
-                electionChoices.map(choice => {
-                  return <>
-                    <div>{choice}</div>
-                    <button>-</button>
+                electionChoices.length == 0 ?
+                  <></>
+                  :
+                  <>
+                    <div className="info-title">Current choices</div>
+                    <div className="choices-container">
+                      {
+
+                        electionChoices.map((choice, i) => {
+                          if (electionChoices.length - 1 == i) {
+                            return <>
+                              <div>{choice}</div>
+                              <button>-</button>
+                            </>
+                          }
+                          else {
+                            return <>
+                              <div>{choice}</div>
+                              <button>-</button>
+                              <hr />
+                            </>
+                          }
+
+                        })
+
+
+                      }
+                    </div>
                   </>
-                })
               }
+
             </>
 
           }
@@ -84,16 +118,16 @@ function CreateElection() {
 
             <label htmlFor="election-title">Tile:</label>
 
-            <input title="election-title" placeholder="What's the election about?" onChange={(e) => setElectionTitle(e.target.value)}></input>
+            <input title="election-title" placeholder="What's the election about?"  onChange={(e) => setElectionTitle(e.target.value)}></input>
 
             <label htmlFor="election-choice">Option:</label>
             <input title="election-choice" placeholder="Enter name of option" ref={choiceInput} onChange={(e) => setChoiceName(e.target.value)}></input>
 
-            <button className="add-button" onClick={(e) => {
+            <button className="add-button" disabled={choiceName == ""}  onClick={(e) => {
               e.preventDefault();
               setElectionChoices([...electionChoices, choiceName])
               choiceInput.current.value = ""
-
+              setChoiceName("")
             }
 
             }
@@ -102,13 +136,15 @@ function CreateElection() {
             <label htmlFor="election-voter">Voter:</label>
             <input title="election-voter" placeholder="Enter name of voter" onChange={(e) => setVoterName(e.target.value)} ref={voterInput}></input>
 
-            <button className="add-button" onClick={(e) => {
+            <button className="add-button" disabled={voterName
+             == ""}  onClick={(e) => {
               e.preventDefault();
               setElectionVoters([...electionVoters, voterName])
               voterInput.current.value = ""
+              setVoterName("")
             }}>Add</button>
 
-            <button onClick={(e) => {
+            <button className="submit-button" disabled={canSave()} onClick={(e) => {
               e.preventDefault();
               console.log(createElection(electionTitle, electionChoices, electionVoters))
             }}>Create</button>
@@ -119,6 +155,43 @@ function CreateElection() {
 
 
         <div className="voter-info">
+          {
+            <>
+              {
+                electionVoters.length == 0 ?
+                  <></>
+                  :
+                  <>
+                    <div className="info-title">Current choices</div>
+                    <div className="voters-container">
+                      {
+
+                        electionVoters.map((voter, i) => {
+                          if (electionVoters.length - 1 == i) {
+                            return <>
+                              <div>{voter}</div>
+                              <button>-</button>
+                            </>
+                          }
+                          else {
+                            return <>
+                              <div>{voter}</div>
+                              <button>-</button>
+                              <hr />
+                            </>
+                          }
+
+                        })
+
+
+                      }
+                    </div>
+                  </>
+              }
+
+            </>
+
+          }
 
         </div>
 
