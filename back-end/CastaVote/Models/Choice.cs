08ca353b7dc731ada0f19b7d072a685c9692e3d4
chris-wamae <chriswamae123@gmail.com>
2024-01-29 11:01:36 +0300

    Added backend

---
+++ back-end/CastaVote/Models/Choice.cs
@@ -0,0 +1,13 @@
+﻿namespace CastaVote.Models
+{
+    public class Choice
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public  Election Election { get; set; }
+
+        public ICollection<Vote> Votes { get; set; }
+    }
+}
