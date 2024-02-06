f24f194923a585d2a06c667273f6e7ff487946db
chris-wamae <chriswamae123@gmail.com>
2024-02-06 15:02:46 +0300

    Created adding mapping through AutoMapper, Added Repositories, AutoMapper and Controllers to Program file

---
+++ back-end/KindleDecision/Dto/ElectionDto.cs
@@ -0,0 +1,18 @@
+﻿namespace KindleDecision.Dto
+{
+    public class ElectionDto
+    {
+
+        public string Title { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public int TotalVotes { get; set; }
+
+        public int RemainingVotes { get; set; }
+
+        public int CreatorUserId { get; set; }
+    }
+}
---
+++ back-end/KindleDecision/Dto/VoteDto.cs
@@ -0,0 +1,9 @@
+﻿using KindleDecision.Models;
+
+namespace KindleDecision.Dto
+{
+    public class VoteDto
+    {
+        public int VoterUserId { get; set; }
+    }
+}
