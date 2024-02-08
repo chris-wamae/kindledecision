a0e8c32bf6ef1d331112d3a38b59b08aa6f57569
chris-wamae <chriswamae123@gmail.com>
2024-02-08 15:50:02 +0300

    Created Choice controller and added requests to it

--- back-end/KindleDecision/Controllers/ChoiceController.cs
+++ back-end/KindleDecision/Controllers/ChoiceController.cs
@@ -1,6 +1,179 @@
-﻿namespace KindleDecision.Controllers
+﻿using KindleDecision.Models;
+using KindleDecision.Interfaces;
+using KindleDecision.Dto;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KindleDecision.Controllers
 {
-    public class ChoiceController
+    [Route("election/choice")]
+    [ApiController]
+    public class ChoiceController : Controller
     {
+        private readonly IChoiceRepository _choiceRepository;
+        private readonly IElectionRepository _electionRepository;
+        private readonly IMapper _mapper;
+
+        public ChoiceController(IChoiceRepository choiceRepository, IMapper mapper, IElectionRepository electionRepository)
+        {
+            _choiceRepository = choiceRepository;
+            _mapper = mapper;
+            _electionRepository  = electionRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Choice>)]
+
+        public IActionResult GetAllChoices() 
+        {
+        var choices = _mapper.Map<List<ChoiceDto>>(_choiceRepository.GetAllChoices());
+        
+        if(!ModelState.IsValid) 
+            {
+            return BadRequest(ModelState);
+            }
+
+        return Ok(choices);
+
+        }
+
+        [HttpGet("{choiceId}")]
+        [ProducesResponseType(200, Type = typeof(Choice))]
+
+        public IActionResult GetChoice(int id) 
+        {
+
+        if(_choiceRepository.ChoiceExists(id))
+            {
+                return NotFound();
+            }
+
+        var choice = _mapper.Map<ChoiceDto>(_choiceRepository.GetChoice(id));
+
+        if(!ModelState.IsValid)
+           return BadRequest(ModelState);
+
+        return Ok(choice);        
+
+        }
+
+        [HttpGet("get-election-choices/{electionId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Choice>))]
+
+        public IActionResult GetChoicesByElection(int electionId) 
+        {
+
+         var choices = _mapper.Map<List<ChoiceDto>>(_choiceRepository.GetChoicesByElection(electionId));
+        
+         if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+         return Ok(choices);
+
+        }
+
+
+        [HttpGet("get-choices-by-user-vote/{userId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Choice>))]
+
+        public IActionResult GetChoicesByUserId(int userId) 
+        {
+        var choices = _mapper.Map<List<ChoiceDto>>(_choiceRepository.GetChoicesByUserVote(userId));
+
+        if(ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+        return Ok(choices);
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+
+        public IActionResult CreateChoice([FromQuery] int electionId, [FromBody] ChoiceDto choiceCreate)
+        {
+            if (choiceCreate == null)
+                return BadRequest(ModelState);
+
+          
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);  
+            }
+
+            var choiceMap = _mapper.Map<Choice>(choiceCreate);
+
+            if(!_choiceRepository.CreateChoice(electionId, choiceMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving the Choice");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok(choiceMap);   
+
+        }
+
+        [HttpPut("{choiceId}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+
+        public IActionResult UpdateGame(int choiceId, ChoiceDto updateChoice)
+        {
+         if(updateChoice == null)
+            {
+            return BadRequest(ModelState);  
+            }
+
+            if (!_choiceRepository.ChoiceExists(choiceId))
+                return NotFound();
+
+            if(choiceId != updateChoice.Id)
+                return BadRequest(ModelState);
+
+            var updatedChoiceMap = _mapper.Map<Choice>(updateChoice);
+
+            if(!_choiceRepository.UpdateChoice(choiceId, updatedChoiceMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while updating the Choice");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok(updatedChoiceMap);
+
+        }
+
+
+        [HttpDelete("{choideId}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+
+        public IActionResult DeleteChoice(int choiceId) 
+        {
+            if(!_choiceRepository.ChoiceExists(choiceId))
+                return NotFound();
+
+            var choiceToDelete = _choiceRepository.GetChoice(choiceId);
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if(!_choiceRepository.DeleteChoice(choiceToDelete))
+                {
+                ModelState.AddModelError("", "Something went wrong while Deleting");
+                return StatusCode(500, ModelState);
+                }
+
+            return NoContent();
+        }
+
     }
 }
--- back-end/KindleDecision/Dto/ChoiceDto.cs
+++ back-end/KindleDecision/Dto/ChoiceDto.cs
@@ -3,6 +3,7 @@
     public class ChoiceDto
     {
 
+    public int Id { get; set; }
     public string Title { get; set; }
     }
 }
