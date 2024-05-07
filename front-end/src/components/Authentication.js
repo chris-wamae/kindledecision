f7de598b34afe15cc2c4ae6370169d75b4953d96
chris-wamae <chriswamae123@gmail.com>
2024-05-07 16:08:57 +0300

    Fixed bugs and updated Authentication.js

--- front-end/src/components/Authentication.js
+++ front-end/src/components/Authentication.js
@@ -10,7 +10,7 @@ import { emailToolTipRenderer } from "../Helper/Form";
 import { changeUserId } from "../features/idSlice";
 import { useSelector } from "react-redux";
 import { loginPost, loginState } from "../features/loginSlice";
-import { signUpState,signupPost } from "../features/signupSlice";
+import { signUpState, signupPost } from "../features/signupSlice";
 import { useNavigate } from "react-router-dom";
 
 //make email database search depend on physical button press by user
@@ -23,19 +23,15 @@ function Authetication({ authType, authTitle, passwordHeader, buttonText }) {
     const loggedUser = useSelector(loginState);
     const signupState = useSelector(signUpState);
     const navigate = useNavigate()
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [emailState, setEmailState] = useState(undefined)
-    const [signupUser, setSignupUser] = useState({
-        email: "",
-        password: "",
-        username: "",
-        phone: "",
-        //boolean
-        viewingmode: undefined,
-        //boolean
-        userVisibility: undefined
-    })
+    const [username, setUsername] = useState("")
+    const [phone, setPhone] = useState("")
+    const [viewmode, setViewmode] = useState(undefined)
+    const [userVisibility, setUserVisibility] = useState(undefined)
 
     useEffect(() => {
         if (validateEmail(email)) {
@@ -78,18 +74,18 @@ function Authetication({ authType, authTitle, passwordHeader, buttonText }) {
 
     }, [loggedUser])
 
-     const createUser = () => {
+    const createUser = () => {
         dispatch(signupPost({
-       firstName:"Mr",
-       lastName:"Nobody",
-       username:signupUser.username,
-       email:email,
-       phone:signupUser.phone,
-       language:"en" ,
-       password:password,
-       viewingmode:signupUser.viewingmode,
-       userVisibility:signupUser.userVisibility,
-       roles:["user"]
+            "firstName": firstName,
+            "lastName": lastName,
+            "username": username,
+            "email": email,
+            "phone": phone,
+            "language": "en",
+            "password": password,
+            "viewmode": viewmode,
+            "userVisibility": userVisibility,
+            "roles": ["user"]
         }))
     }
 
@@ -97,6 +93,7 @@ function Authetication({ authType, authTitle, passwordHeader, buttonText }) {
     //     //|| foundUser.length == 0
 
     // }, [fo])
+    //console.log(signupUser.viewingmode)
 
     return (
         <>
@@ -112,29 +109,43 @@ function Authetication({ authType, authTitle, passwordHeader, buttonText }) {
                     <form className="auth-form">
                         <p className="input-header">Your email address:</p>
                         {emailToolTipRenderer(emailState)}
-                        <input placeholder="email address" type="email" onChange={(e) => setEmail(e.target.value)}></input>
-                        <p className="input-header" id="password-header"><br></br></p>
-                        <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)}></input>
+                        <input placeholder="Email address" type="email" onChange={(e) => setEmail(e.target.value)}></input>
+                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
                         {
                             authType ?
-                                <div className="sign-up-container">
-                                    <input type="text" placeholder="username" onChange={(e) => setSignupUser({ ...signupUser, username: e.target.value })}></input>
-                                    <input type="text" placeholder="phone" onChange={(e) => setSignupUser({ ...signupUser, phone: e.target.value })}></input>
+                                <span className="sign-up-container">
+                                    <input type="text" placeholder="First name" onChange={(e) => setFirstName(e.target.value)}></input>
+                                    <input type="text" placeholder="Last name" onChange={(e) => setLastName(e.target.value)}></input>
+                                    <input type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)}></input>
+                                    <input type="text" placeholder="phone" onChange={(e) => setPhone(e.target.value)}></input>
                                     <select onChange={e => {
-                                        console.log(e.target.value);
-                                        setSignupUser({ ...signupUser, viewingmode:e.target.value })
-                                }}>
+                                        if (e.target.value == "true") {
+                                            setViewmode(true)
+                                        }
+                                        else if (e.target.value == "false") {
+                                            setViewmode(false)
+                                        }
+                                    }}>
                                         <option>Viewing mode</option>
-                                        <option value={false}>Darkmode</option>
-                                        <option value={true}>Lightmode</option>
+                                        <option value={"false"}>Darkmode</option>
+                                        <option value={"true"}>Lightmode</option>
                                     </select>
-                                    <select onChange={e => setSignupUser({ ...signupUser, uservisibility:e.target.value })}>
+                                    <select onChange={e => {
+                                        if (e.target.value == "true") {
+                                            setUserVisibility(true)
+                                        }
+                                        else if (e.target.value == "false") {
+                                            setUserVisibility(false)
+                                        }
+                                    }
+
+                                    }>
                                         <option>Selection visibility</option>
-                                        <option value={false}>Anonymous</option>
-                                        <option value={true}>Visible</option>
+                                        <option value={"false"}>Anonymous</option>
+                                        <option value={"true"}>Visible</option>
                                     </select>
 
-                                </div> :
+                                </span> :
                                 <span></span>
                         }
                         <button onClick={(e) => {
@@ -142,9 +153,8 @@ function Authetication({ authType, authTitle, passwordHeader, buttonText }) {
                             if (!authType) {
                                 searchUser()
                             }
-                            else if(authType)
-                            {
-                             createUser();
+                            else if (authType) {
+                                createUser();
                             }
 
                         }}>{buttonText}</button>
