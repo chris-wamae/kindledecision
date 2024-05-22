2d23b9bd95c6086b702432ab205f2a2dbd8ef061
chris-wamae <chriswamae123@gmail.com>
2024-05-22 12:08:01 +0300

    Created email updated auth route

--- back-end/KindleDecision/Models/ApplicationUser.cs
+++ back-end/KindleDecision/Models/ApplicationUser.cs
@@ -4,13 +4,11 @@ namespace KindleDecision.Models
 {
     public class ApplicationUser : IdentityUser
     {
-     public int UserId { get; set; }
+    public int UserId { get; set; }
 
     public string? RefreshToken { get; set; }
 
     public DateTime RefreshTokenExpiryTime { get; set; }
-
-
 
     }
 }
--- back-end/KindleDecision/Models/User.cs
+++ back-end/KindleDecision/Models/User.cs
@@ -4,10 +4,7 @@
     {
         public int Id { get; set; }
         public string Email { get; set; }
-        public string Language { get; set; }
-
-        public string Password { get; set; } 
-        
+        public string Language { get; set; }        
         public bool Viewmode { get; set; }
 
         public bool UserVisibility { get; set; }
