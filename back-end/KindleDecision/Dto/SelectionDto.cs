9225ee6d235d12cc9229fd63b7bd1b8c0acdd4b7
chris-wamae <chriswamae123@gmail.com>
2024-06-24 16:11:50 +0300

    Updated Models and Controllers to allow Descriptions to be added for Queries and Choices and reasons to be added for Selections

--- back-end/KindleDecision/Dto/SelectionDto.cs
+++ back-end/KindleDecision/Dto/SelectionDto.cs
@@ -5,6 +5,8 @@ namespace KindleDecision.Dto
     public class SelectionDto
     {   
         public int Id { get; set; } 
+
+        public string Reason { get; set; }
         public int SelectorUserId { get; set; }
     }
 }
